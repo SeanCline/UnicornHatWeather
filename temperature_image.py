dc79e7f926b538772a8dd07b5918fa6bb07194ae
Sean D. Cline <sean.d.cline@gmail.com>
2020-09-22 08:36:05 -0400

    Add some comments.

--- temperature_image.py
+++ temperature_image.py
@@ -2,12 +2,14 @@
 from PIL import Image, ImageChops
 import colorsys
 
+# Returns an image representing a given character.
 def open_char_image(character : str):
     if len(character) != 1:
         raise RuntimeError('Not a character.')
     return Image.open(f'characters/{ord(character)}.gif').convert('RGB')
 
 
+# Draws a string of fixed width characters on to an image at the given location.
 def draw_msg_on_image(base_img, msg : str, x : int, y : int, padding : int = 1):
     img = base_img.copy()
     for c in msg:
@@ -17,28 +19,32 @@ def draw_msg_on_image(base_img, msg : str, x : int, y : int, padding : int = 1):
     return img
 
 
+# Saturate a value to a given min or max.
 def clamp(val, low, high):
     return max(min(val, high), low)
 
 
+# Linear interpolate a value x from one range of values into another.
 def interpolate(x : float, src_range, dest_range):
     frac = x / (src_range[1] - src_range[0])
     return dest_range[0] + frac * (dest_range[1] - dest_range[0])
 
 
+# Converts a Fahrenheit temperature into an associated RGBA colour. Blue for cold. Red for hot.
 def tempertature_to_color(temperature : float):
     temp = clamp(temperature, 32, 100)
     hue = interpolate(temp, (32, 100), (0, .68))
     color = colorsys.hsv_to_rgb(1-hue, 1.0, 1.0)
-    return (int(color[0]*255), int(color[1]*255), int(color[2]*255), int(1*255))
+    return (int(color[0]*255), int(color[1]*255), int(color[2]*255), 255)
 
 
-def apply_temperature_tint_to_image(base_img, tempurature : int):
-    print(tempertature_to_color(tempurature))
-    filter_img = Image.new('RGB', (base_img.width, base_img.height), tempertature_to_color(tempurature))
+# Tints an image with a given colour value.
+def apply_color_filter_to_image(base_img, color):
+    filter_img = Image.new('RGB', (base_img.width, base_img.height), color)
     return ImageChops.multiply(base_img, filter_img)
 
 
+# Creates an 8x8 image with the current temperature on it.
 def create_temperature_image(temperature : int):
     temp_str = str(temperature).zfill(2)
 
@@ -50,7 +56,7 @@ def create_temperature_image(temperature : int):
     # Draw the temperature on top of the base image.
     img = Image.open('icons/degree_background.gif').convert('RGB')
     img = draw_msg_on_image(img, temp_str, 0, 3)
-    img = apply_temperature_tint_to_image(img, temperature)
+    img = apply_color_filter_to_image(img, tempertature_to_color(temperature))
     
     return img
 
